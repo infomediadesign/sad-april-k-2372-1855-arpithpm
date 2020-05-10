794a39d077a58e09fa406699c5d9585295a64ccb
Arpith Muddi <arpithpm@gmail.com>
2020-05-10 11:11:19 +0200

    Backend Testcases
    Backend Testcases.

--- Server-Backend/djangoforstays/settings.py
+++ Server-Backend/djangoforstays/settings.py
@@ -112,6 +112,13 @@ DATABASES = {
     }
 }
 
+import sys
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase'
+    }
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
--- Server-Backend/domigo/tests.py
+++ Server-Backend/domigo/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+import requests
+import json
 
-# Create your tests here.
+
+class AnimalTestCase(TestCase):
+
+    def test_check_pet_categories(self):
+        """Animals that can speak are correctly identified"""
+        a = requests.get("http://127.0.0.1:8000/api/petcategory/")
+        self.assertEqual(a.status_code, 200)
+
+    def test_check_length_of_pet_categories(self):
+        """Animals that can speak are correctly identified"""
+        a = requests.get("http://127.0.0.1:8000/api/petcategory/")
+        a = json.loads(a.text)
+        self.assertEqual(len(a), 2)
+
+    def test_cat_exists_in_categories(self):
+        """Animals that can speak are correctly identified"""
+        a = requests.get("http://127.0.0.1:8000/api/petcategory/")
+        self.assertIn("Cat", a.text)
+
+    def test_payment_methods_len(self):
+        a = requests.get("http://127.0.0.1:8000/api/paymentmethods/").json()
+        self.assertEqual(len(a), 1)
+
+    def test_payment_methods_exists(self):
+        a = requests.get("http://127.0.0.1:8000/api/paymentmethods/").text
+        self.assertIn("Cash On Delivery", a)
